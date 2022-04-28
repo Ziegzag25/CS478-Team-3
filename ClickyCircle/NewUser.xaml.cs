3929f304b2aec46da98755fba613676f281cbf52
jkujawa2@gmail.com <jkujawa2@gmail.com>
2022-04-28 01:25:52 -0500

    Added account creatin function

--- ClickyCircle/NewUser.xaml.cs
+++ ClickyCircle/NewUser.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,13 +21,13 @@ namespace ClickyCircle
     /// </summary>
     public partial class NewUser : Window
     {
+        DataTable dt = new DataTable();
         public NewUser()
         {
             InitializeComponent();
         }
 
         
-
         private void BTNConfirm_Click(object sender, RoutedEventArgs e)
         {
             //needs way to save user data
@@ -39,14 +41,43 @@ namespace ClickyCircle
             }
             //"" is not a perfect solution, it can be easily bypassed
             //goes for login page too 
-            else if(PWDPassword.Password == "" || PWDPasswordCheck.Password =="")
-                {
+            else if (PWDPassword.Password == "" || PWDPasswordCheck.Password == "")
+            {
                 MessageBox.Show("Please enter a Username/Password");
 
             }
             else
             {
+                //create a data table with desired columns
+
+                dt.TableName = "Userstable";
+                dt.Columns.Add("User_Name");
+                dt.Columns.Add("Password");
+                dt.Columns.Add("Score");
+
+                //add the rows needed
+                DataRow dr;
+                dr = dt.NewRow();
+                dr["User_Name"] = TBUserName.Text;//gets user name and passowrd from tbs
+                dr["Password"] = PWDPassword.Password;
+                dr["Score"] = 0;
+
+
+                dt.Rows.Add(dr);
+                string Users_Scores = @"User_Scores.xmal";
+                if (!File.Exists(Users_Scores))
+                {
+                    File.Create(Users_Scores);
+                }
+                else
+                    dt.WriteXml("User_Scores.xmal");
+                MainMenu mm = new MainMenu();
+               
+             
+                mm.Show();
+
                 this.Close();
+
             }
         }
 
@@ -56,3 +87,4 @@ namespace ClickyCircle
         }
     }
 }
+
